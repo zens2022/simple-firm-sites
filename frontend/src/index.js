e71b1d431e52c2adae3687d3cb99e41c9b638c5c
zens <kamicreator@gmail.com>
2023-09-28 22:48:25 +0800

    [frontend][add] create react app
    - create react app
    - install dependencies
        - mui
        - mui material icons
        - axios
        - react router

---
+++ frontend/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+const element = document.getElementById('app');
+const root = ReactDOM.createRoot(element);
+root.render(
+  <React.StrictMode>
+    SIMPLE FIRM SITE
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
